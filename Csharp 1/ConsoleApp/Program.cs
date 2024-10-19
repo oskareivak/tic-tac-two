a7d75c1635e0b700bce2c6d4773f451b0f0d5506
oseiva <oseiva@taltech.ee>
2024-10-19 15:13:34 +0300

    changes

--- Csharp 1/ConsoleApp/Program.cs
+++ Csharp 1/ConsoleApp/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using BLL;
-
-Console.WriteLine("Hello World!");
-
-var calc = new CalculatorBrain();
-calc.Number1 = 1;
-calc.Number2 = 2;
-var.res:int = calc.Add();
-
-Console.WriteLine($"{calc.Number1} + {calc.Number2} = {res}");
+// using BLL;
+//
+// Console.WriteLine("Hello World!");
+//
+// var calc = new CalculatorBrain();
+// calc.Number1 = 1;
+// calc.Number2 = 2;
+// var.res:int = calc.Add();
+//
+// Console.WriteLine($"{calc.Number1} + {calc.Number2} = {res}");
