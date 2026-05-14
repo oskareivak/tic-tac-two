f81957062f3677dd81b668e0691735686bcf7a9e
oseiva <oseiva@taltech.ee>
2026-05-14 18:50:30 +0300

    Add docker deploy and github actions workflow

--- tic-tac-toe/tic-tac-toe/DAL/FileHelper.cs
+++ tic-tac-toe/tic-tac-toe/DAL/FileHelper.cs
@@ -2,9 +2,24 @@ namespace DAL;
 
 public static class FileHelper
 {
-    public static string BasePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-                                    + Path.DirectorySeparatorChar + "tic-tac-two" + Path.DirectorySeparatorChar;
+    public static string BasePath { get; } = ResolveBasePath();
 
     public static string ConfigExtension = ".config.json";
     public static string GameExtension = ".game.json";
+
+    private static string ResolveBasePath()
+    {
+        var envPath = Environment.GetEnvironmentVariable("TICTACTWO_DATA_DIR");
+        var basePath = string.IsNullOrWhiteSpace(envPath)
+            ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "tic-tac-two")
+            : envPath.Trim();
+
+        if (!basePath.EndsWith(Path.DirectorySeparatorChar))
+        {
+            basePath += Path.DirectorySeparatorChar;
+        }
+
+        Directory.CreateDirectory(basePath);
+        return basePath;
+    }
 }
